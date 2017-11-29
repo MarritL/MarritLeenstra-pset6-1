2580baabfbc3df9082555cc1d841963547dc5ae3
Marrit <marritleenstra@hotmail.com>
2017-11-29 18:20:25 +0100

    started to add fragements to the mainActivity

--- app/src/main/java/marrit/marritleenstra_pset6_1/MainActivity.java
+++ app/src/main/java/marrit/marritleenstra_pset6_1/MainActivity.java
@@ -3,11 +3,13 @@ package marrit.marritleenstra_pset6_1;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     private TextView mTextMessage;
 
@@ -18,7 +20,17 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                    // create new fragment
+                    HomeFragment homeFragment = new HomeFragment();
+
+                    // add the fragment to the 'fragment_container' framelayout
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                    transaction.replace(R.id.fragment_container, homeFragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+
                     return true;
                 case R.id.navigation_user:
                     mTextMessage.setText(R.string.title_user);
@@ -37,7 +49,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        // create new fragment
+        HomeFragment homeFragment = new HomeFragment();
+        // add the fragment to the 'fragment_container' framelayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, homeFragment).commit();
+
+        //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
